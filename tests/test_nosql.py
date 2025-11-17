e416581140a22f15614db39c12f2f4edf1ffd4b6
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-11-17 13:27:58 -0600

    Update test_nosql.py

--- tests/test_nosql.py
+++ tests/test_nosql.py
@@ -29,6 +29,6 @@ def test_red_subordinados():
 def test_red_rutas():
   result = run_query("SELECT DISTINCT * FROM alcanzables_view;")
   print(f'resultado del query: {result}')
-  rutas={row[0] for row in result}
+  rutas={row[1] for row in result}
   assert len(rutas)==9
   
